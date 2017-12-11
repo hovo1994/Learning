82f96b3558e51288741bcbeb0a1919830ab8f4bf
hovo1994 <hovhannes1894@gmail.com>
2017-12-11 14:39:02 -0500

    Initialize Learning
    I hope this can inspire others to never stop learning.

---
+++ learning.c
@@ -0,0 +1,12 @@
+//     File: Learning.c
+//   Author: Everyone
+//  Purpose: To Inspire
+// Revision: 0.0.0
+
+void main(faith) {
+    while(true) {
+        read();
+        think();
+        write();
+    }
+}
