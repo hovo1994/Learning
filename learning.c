95328d804f5e082c05e347a296a5cb19a7774518
hovo1994 <hovhannes1894@gmail.com>
2017-12-16 06:12:47 -0500

    Update learning.c
    insert sleep to continue :)

--- learning.c
+++ learning.c
@@ -1,12 +1,21 @@
 //     File: Learning.c
 //   Author: Everyone
 //  Purpose: To Inspire
-// Revision: 0.0.0
+// Revision: 0.0.1
 
 void main(faith) {
     while(true) {
         read();
         think();
         write();
+        else if(tired){
+            sleep();
+        }
+        if(hungry){
+            eat();
+        }
+        if(needToPoop){
+            poop();
+        }
     }
 }
